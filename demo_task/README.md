c51014e8a7d772120323f626dd2964fbd3fd3a1f
Ivan Mosiagin <mosiaginip@gmail.com>
2024-06-07 12:20:34 -0700

    new logic addded - regexp filter and file check

--- demo_task/README.md
+++ demo_task/README.md
@@ -6,6 +6,10 @@ This is a simple application to analyser running processes on **Windows** machin
 1. Ensure that Docker Daemon is running.  
 2. Open the terminal in the project folder. *Commands are for PowerShell terminal*  
 3. Build Docker Image `docker build -t process-analyser .`  
-4. Run Docker: `docker run --rm --pid=host -v /:/hostfs <Path_to_save_JSON>:/app process-analyser`, e.g.:  
-`docker run --rm --pid=host -v /:/hostfs -v /c/Users/Ivan/Dropbox/pro/Danilveich/demo_task:/app process-analyser`  
+4. Run Docker: `docker run --rm --pid=host -v /:/hostfs <Path_to_save_JSON>:/app -it process-analyser`, e.g.:  
+`docker run --rm --pid=host -v /:/hostfs -v /c/Users/Ivan/Dropbox/pro/Danilveich/modern_infrastructure/demo_task:/app -it process-analyser`  
 5. Enjoy your freshly generated JSON!
+
+## Details  
+One can provide regexp expression to filter names, to use the expression put the files in files_to_check folder.  
+Also, if one would like to filter generated patterns by name with some regular expression, feel free to modify .env file.  
--- demo_task/process_analyser.py
+++ demo_task/process_analyser.py
@@ -3,6 +3,18 @@ import json
 import psutil
 from datetime import datetime
 from math import ceil, sqrt
+import dotenv
+import re
+
+def load_pattern():
+    """
+    Load pattern from .env file
+    """
+    dotenv.load_dotenv()
+    pattern_str = os.getenv('REGEXP_NAME_PATTERN')
+
+    return re.compile(pattern_str)
+
 
 def is_number_prime(number):
     """
@@ -29,12 +41,43 @@ def clean_process_name(name):
     cleaned_name = ''.join(char for char in name if char.isprintable())
     return os.path.basename(cleaned_name.split()[0])
 
-def main():
+def get_input_from_user():
+    """
+    Get input from user on how we want to proceed
+    """
+    input_on_mode = input('Do you want to generate or check? (g for generate): ')
+    if input_on_mode != 'g':
+        mode = 'check'
+        return mode, False
+    else:
+        mode = 'generate'
+
+    input_on_regexp = input('Do you want to use regexp for process name? (y for yes): ')
+    if input_on_regexp == 'y':
+        use_regexp_flg = True
+    else:
+        use_regexp_flg = False
+
+    return mode, use_regexp_flg
+
+
+def run_generation_mode(use_regexp_flg):
+    """
+    Function to generate JSON with description of processes
+    """
+    regexp_name_pattern = load_pattern()
     list_processes = []
+    counter_process = 0
+
     for process in psutil.process_iter():
+        counter_process += 1
         try:
             process_info = process.as_dict(attrs=['pid', 'name', 'cmdline'])
             process_name = os.path.basename(process_info['name'])
+
+            if use_regexp_flg and not regexp_name_pattern.match(process_name):
+                continue
+
             is_prime = is_number_prime(process_info['pid'])
             dict_for_now = {'Process name': clean_process_name(process_name),
                             'Process ID': process_info['pid'],
@@ -44,10 +87,72 @@ def main():
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
 
-    with open(make_name_for_json(), 'w') as f:
+    if use_regexp_flg:
+        print(f'Processes that match the pattern: {len(list_processes)} out of {counter_process}', flush=True)
+
+    folder_name = "results_of_generation"
+    file_name = make_name_for_json()
+    file_path = os.path.join(folder_name, file_name)
+
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    with open(file_path, 'w') as f:
         json.dump(list_processes, f,  indent=4)
 
     print('Your JSON is ready!', flush=True)
+    
+def check_processes_from_json(processes):
+    """
+    To check if processes are prime or not and compare with the list of processes
+    """
+    error_count = 0
+    error_messages = []
+    try:
+        for process in processes:
+            pid = process["Process ID"]
+            if process["Is Prime"] != is_number_prime(process["Process ID"]):
+                error_count += 1
+                if process["Is Prime"]:
+                    first_word, second_word = 'составное', 'простое'
+                else:
+                    first_word, second_word = 'простое', 'составное'
+        
+                error_messages.append(
+                    f"________Для {process['Process name']} процесса найдена ошибка."
+                    f"\n________Число(PID) {pid} на самом деле {first_word}, а в файле написано {second_word}."
+                )
+        
+        if error_count > 0:
+            error_messages.append(f"____В файле найдены ошибки: {error_count} штук_и")
+            for error in error_messages:
+                    print(error, flush=True)
+        else:
+            print("____Ошибок не найдено", flush=True)
+    except:
+        print("____Ошибка при чтении файла")
+        
+    return
+    
+def run_check_mode():
+    """
+    Function to check if process is prime
+    """
+    for file in os.listdir('files_to_check'):
+        with open(f'files_to_check/{file}', 'r') as f:
+            processes = json.load(f)
+            print(f'File {file} analysis:', flush=True)
+            check_processes_from_json(processes)
+            
+
+def main():
+    mode, use_regexp_flg = get_input_from_user()
+
+    if mode == 'check':
+        run_check_mode()
+    else:
+        run_generation_mode(use_regexp_flg)
+    
 
 if __name__ == '__main__':
     main()
