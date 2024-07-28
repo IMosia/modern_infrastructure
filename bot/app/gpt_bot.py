4ac504dcc3e41b07d27cd3696ea99a8c37313a5a
Ivan Mosiagin <mosiaginip@gmail.com>
2024-07-28 10:57:56 -0700

    pylint fixes

--- bot/app/gpt_bot.py
+++ bot/app/gpt_bot.py
@@ -1,17 +1,20 @@
-from dotenv import load_dotenv
+"""
+This is orchestrator for telegram bot commands and communication
+"""
+
 import os
-from openai import OpenAI
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, Updater, ConversationHandler, CallbackContext, CallbackQueryHandler
 import json
 import logging
 
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import (Application, CommandHandler, MessageHandler, filters
+                          , ConversationHandler, CallbackQueryHandler)
 
-from src.commands import start, world_time_now, provide_picture_and_ask_prompt, start_picture_command, start_recording_meeting
+from src.commands import (start, world_time_now, provide_picture_and_ask_prompt
+                          , start_picture_command, start_recording_meeting)
 from src.gpt_handler import message_acrhistator
-from src.decorators import decorator_logging, decorator_check_if_user_is_allowed
 from src.collection_of_info import handle_meeting_type, handle_meeting_name
-
 
 
 # env variables
@@ -24,13 +27,14 @@ MEETING_STATE = 2
 MEETING_STATE_SECOND = 3
 
 # config from config.json
-with open('config.json', 'r') as file:
+with open('config.json', 'r', encoding='utf-8') as file:
     config = json.load(file)
 model_version = config['model_version']
 model_constant = config['model_constant']
 important_timezones = config['important_timezones']
 
 def main():
+    """Main"""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(BOT_TOKEN).build()
     logging.info(f"Bot {BOT_NAME} started.")
@@ -38,12 +42,14 @@ def main():
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("current_time",
-                                            lambda update, context: world_time_now(important_timezones, update, context)))
+                                lambda update
+                                , context: world_time_now(important_timezones, update, context)))
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('picture', start_picture_command)],
         states={
-            PROMPT_STATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, provide_picture_and_ask_prompt)],
+            PROMPT_STATE: [MessageHandler(filters.TEXT & ~filters.COMMAND
+                                          , provide_picture_and_ask_prompt)],
         },
         fallbacks=[]
     )
@@ -53,7 +59,8 @@ def main():
         entry_points=[CommandHandler('record_meeting', start_recording_meeting)],
         states={
             MEETING_STATE: [CallbackQueryHandler(handle_meeting_type)],
-            MEETING_STATE_SECOND: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_meeting_name)],
+            MEETING_STATE_SECOND: [MessageHandler(filters.TEXT & ~filters.COMMAND
+                                                  , handle_meeting_name)],
         },
         fallbacks=[]
     )
@@ -63,7 +70,7 @@ def main():
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
-    
-    
+
+
 if __name__ == '__main__':
     main()
